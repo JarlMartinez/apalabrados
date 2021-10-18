98c9855e18c6c226f3e34f790dcd8dd5bcd3bb1a
Luis Martinez <luis.martinez@venafi.com>
2021-10-18 12:33:15 -0500

    reading . Use defautl if not set

--- main.go
+++ main.go
@@ -3,16 +3,26 @@ package main
 import (
 	"apalabrados/controllers"
 	"apalabrados/models"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		fmt.Println("$PORT not set. Using defautl")
+		port = "8080"
+	}
+
 	models.ConnectDB()
 
 	http.Handle("/", http.FileServer(http.Dir("./public")))
 	controllers.RegisterControllers()
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 
 }
